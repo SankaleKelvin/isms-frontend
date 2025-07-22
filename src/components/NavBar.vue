2d1cd3f5d310836d883dbf090f022b0f18b11bdd
Kelvin Sankale <kelvin.sankale@strathmore.edu>
2025-07-22 14:49:17 +0300

    Test full automation using jenkins and webhooks

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -36,7 +36,7 @@
     ></v-app-bar-nav-icon>
 
     <v-toolbar-title class="text-uppercase">
-      <span class="font-weight-light">DISSERTATION-TEST-FUllAutomation</span>
+      <span class="font-weight-light">DISSERTATION-TEST-FUllAutomation-Job</span>
     </v-toolbar-title>
 
     <v-spacer></v-spacer>
